3a6fa63374279c186912ff33ef5198d4f2531fba
Jason Neel <jasonneel@yahoo.com>
2012-07-03 17:41:29 -0400

    Adding User Functionality

--- cakephp/app/Controller/AppController.php
+++ cakephp/app/Controller/AppController.php
@@ -32,4 +32,15 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+	public $components = array(
+        'Session',
+        'Auth' => array(
+            'loginRedirect' => array('controller' => 'posts', 'action' => 'index'),
+            'logoutRedirect' => array('controller' => 'pages', 'action' => 'display', 'home')
+        )
+    );
+
+    public function beforeFilter() {
+        $this->Auth->allow('index', 'view');
+    }
 }
--- cakephp/app/View/Posts/edit.ctp
+++ cakephp/app/View/Posts/edit.ctp
@@ -7,3 +7,4 @@
     echo $this->Form->input('body', array('rows' => '3'));
     echo $this->Form->input('id', array('type' => 'hidden'));
     echo $this->Form->end('Save Post');
+ ?>
