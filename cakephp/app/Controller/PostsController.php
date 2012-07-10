a584a7e0a4bacf74892ee4f2642abfcd6ee71e15
Jason Neel <jasonneel@yahoo.com>
2012-07-10 17:00:12 -0400

    Add user_id to post records

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -15,6 +15,7 @@ class PostsController extends AppController {
 
     public function add() {
         if ($this->request->is('post')) {
+        	$this->request->data['Post']['user_id'] = $this->Auth->user('id');
             if ($this->Post->save($this->request->data)) {
                 $this->Session->setFlash('Your post has been saved.');
                 $this->redirect(array('action' => 'index'));
